f29f59a8e538ccf43ac48f1d9874fffd7ccf5596
ush <159524150+ushec@users.noreply.github.com>
2024-03-26 12:51:56 +0000

    Update database.js

--- src/data/database.js
+++ src/data/database.js
@@ -12,8 +12,8 @@ async function waitForDB() {
       // Try to connect to the database
       const pool = mysql.createPool({
         host: process.env.MYSQL_HOST || 'localhost',
-        user: process.env.MYSQL_USER || 'metalbrain',
-        password: process.env.MYSQL_PASSWORD || 'London1983@@@!',
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE || 'world',
       });
 
